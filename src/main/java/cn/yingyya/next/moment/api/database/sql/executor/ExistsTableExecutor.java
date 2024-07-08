f1c8586214e856f13963002d89f49e13295da681
Yingyya <yyaiwy666@163.com>
2024-07-08 18:08:55 +0800

    feat: ExistsTableExecutor.java

---
+++ src/main/java/cn/yingyya/next/moment/api/database/sql/executor/ExistsTableExecutor.java
@@ -0,0 +1,21 @@
+package cn.yingyya.next.moment.api.database.sql.executor;
+
+import cn.yingyya.next.moment.api.database.DataConnector;
+import cn.yingyya.next.moment.api.database.sql.SQLExecute;
+import cn.yingyya.next.moment.api.database.sql.SQLExecutor;
+import com.zaxxer.hikari.HikariDataSource;
+import org.jetbrains.annotations.NotNull;
+
+public class ExistsTableExecutor extends SQLExecutor<Boolean> {
+
+	protected ExistsTableExecutor(@NotNull String table) {
+		super(table);
+	}
+
+	@Override
+	public @NotNull Boolean execute(@NotNull DataConnector<?> connector) {
+		if (!(connector.dataSource() instanceof HikariDataSource dataSource)) return Boolean.FALSE;
+
+		return SQLExecute.hasTable(dataSource, table);
+	}
+}
